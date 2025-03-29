51b86a744762eb7a64c42e10e8865b8881485ebd
yoo-developer <houssemchelihi.000@gmail.com>
2025-03-29 20:00:56 +0100

    Migrate from AWS S3 to Wasabi storage

--- vectorizing/server/s3.py
+++ vectorizing/server/s3.py
@@ -1,7 +1,20 @@
 import cuid
 import boto3
+import os
 
-S3 = boto3.client("s3", region_name="eu-central-1")
+# Get Wasabi credentials from environment variables
+WASABI_ACCESS_KEY = os.getenv('WASABI_ACCESS_KEY')
+WASABI_SECRET_KEY = os.getenv('WASABI_SECRET_KEY')
+WASABI_REGION = os.getenv('WASABI_REGION', 'us-east-1')  # Default to us-east-1 if not specified
+
+# Create S3 client with Wasabi endpoint
+S3 = boto3.client(
+    "s3",
+    endpoint_url=f"https://s3.{WASABI_REGION}.wasabisys.com",
+    aws_access_key_id=WASABI_ACCESS_KEY,
+    aws_secret_access_key=WASABI_SECRET_KEY,
+    region_name=WASABI_REGION
+)
 
 def upload_markup (markup, s3_bucket_name):
     cuid_str = cuid.cuid()
